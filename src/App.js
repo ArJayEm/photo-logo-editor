cd81aa220a2773a533efef1703ee169a7ee86fa7
Raff Julius Martinez <raffjulius.martinez@outlook.com>
2023-08-30 17:02:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import uniqid from "uniqid";
 
 function App() {
+  const acceptImages = ".jpg, .jpeg, .png, .webp";
+  const [files, setFiles] = useState([]);
+
+  const handleOnSelectPhotosChange = (e) => {
+    const selectedFiles = e.currentTarget.files;
+
+    if (selectedFiles && selectedFiles[0]) {
+      setFiles([]);
+      [...selectedFiles].forEach((file, i) => {
+        var id = `Photo_${i}`;
+        setFiles((files) => [...files, { file: file, id: id }]);
+
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = async (f) => {
+          var jpgFile = f.target.result;
+          document.querySelector(`fieldset#SelectedPhotos img#${id}`).src =
+            jpgFile;
+        };
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        id="SelectPhotos"
+        type="file"
+        onChange={handleOnSelectPhotosChange}
+        accept={acceptImages}
+        multiple
+      />
+      <fieldset id="SelectedPhotos">
+        <legend>Selected Photos</legend>
+        {files &&
+          files.map((file, i) => {
+            var image = file.file;
+            console.log(image);
+            return (
+              <img
+                key={uniqid()}
+                id={`${file.id}`}
+                src={image}
+                alt={`${image.name}`}
+              />
+            );
+          })}
+      </fieldset>
+    </>
   );
 }
 
